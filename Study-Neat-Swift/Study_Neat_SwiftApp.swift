f4cf8e8b51ae7cce5f49688ec7462b971e62f8d5
Mohit Ganna <mohitganna@gmail.com>
2024-08-12 22:41:16 -0400

    Initial Commit

---
+++ Study-Neat-Swift/Study_Neat_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_Neat_SwiftApp.swift
+//  Study-Neat-Swift
+//
+//  Created by Mohit Ganna on 12-08-2024.
+//
+
+import SwiftUI
+
+@main
+struct Study_Neat_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
